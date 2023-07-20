6e032e4b29a5f4a99e30572637b40b25859d5a80
ghassanit24-meet <59498852+ghassanit24-meet@users.noreply.github.com>
2023-07-20 18:11:06 +0300

    done

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -6,7 +6,6 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', ) # What methods are needed?
 def home():
-	
 	return render_template('home.html')
 
 
@@ -26,7 +25,18 @@ def display():
 def thanks():
 
 	return render_template('thanks.html')
+@app.route('/submit_quote' , methods=['GET', 'POST'])
+def method():
+	if request.method == 'POST':
+		age = request.form.get('age')
+		login_session['age'] = age
 
+		return redirect(url_for('error.html'))
+	return render_template('home.html')	
+@app.route('/error.html')
+def error():
+	age = login_session.get('age', 'N/A')
+	return render_template('error.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True)When the form is submitted, store the form's information in login_session, example: login_session['age'] = age.
